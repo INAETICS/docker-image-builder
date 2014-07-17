4a7079e4f7fdbc5e24ca1df440159e7ad8b89ead
Jan Willem Janssen <janwillem.janssen@luminis.eu>
2014-07-17 10:47:03 +0200

    Code cleanup:
    - moved all Etcd related code to lib/etcdctl.sh and update the main
      script to only use those functions;
    - fixed a small issue when the cloned repository was removed after
      an image was build, in which case it should clone again and see if
      a build is necessary;
    - simplified the urlencode() function to only use Bash builtins (it
      appears that xxd is not standard on all Linux systems).

--- lib/etcdctl.sh
+++ lib/etcdctl.sh
@@ -11,22 +11,23 @@
 # returns etcdctl exit code
 #
 etcd/keys () {
-  local key=$1
-  local peers=$2
+    local key=$1
+    local peers=${2:-$ETCDCTL_PEERS}
 
-  local code
-  local resp
-  if [ ! "$peers" == "" ]; then
-    resp=($(etcdctl --peers $2 ls $1))
-  else
-    resp=($(etcdctl ls $1))
-  fi
-  if [ $? -eq 0 ]; then
-    echo "${resp[@]}"
-    return 0
-  fi
-  echo "${resp[@]}" 1>&2
-  return 1
+    local code
+    local resp
+    if [ -n $peers ]; then
+        resp=($(etcdctl --peers "$peers" ls $key 2>/dev/null))
+    else
+        resp=($(etcdctl ls $key 2>/dev/null))
+    fi
+    if [ $? -eq 0 ]; then
+        echo "${resp[@]}"
+        return 0
+    else
+        echo "Failed to get $key: ${resp[@]} [$?]!" >&2
+        return 1
+    fi
 }
 
 # Get the value for an etcd key
@@ -37,22 +38,22 @@ etcd/keys () {
 # returns 
 #
 etcd/value () {
-  local key=$1
-  local peers=$2
+    local key=$1
+    local peers=${2:-$ETCDCTL_PEERS}
 
-  local resp
-  local resp
-  if [ ! "$peers" == "" ]; then
-    resp=$(etcdctl --peers $2 get $1)
-  else
-    resp=$(etcdctl get $1)
-  fi
-  if [ $? -eq 0 ]; then
-    echo $resp
-    return 0
-  fi
-  echo $resp 1>&2
-  return 1
+    local resp
+    if [ -n $peers ]; then
+        resp=$(etcdctl --peers "$peers" get $key 2>/dev/null)
+    else
+        resp=$(etcdctl get $key 2>/dev/null)
+    fi
+    if [ $? -eq 0 ]; then
+        echo $resp
+        return 0
+    else
+        # No such value...
+        return 1
+    fi
 }
 
 # List child values for an etcd key
@@ -64,25 +65,71 @@ etcd/value () {
 # returns etcdctl exit code
 #
 etcd/values () {
-  local key=$1
-  local peers=$2
+    local key=$1
+    local peers=${2:-$ETCDCTL_PEERS}
 
-  local keys
-  local code
-  keys=$(etcd/keys $key $peers)
-  if [ $? -gt 0 ] ; then
-    return 1;
-  fi
-
-  local values=()
-  local resp
-  for child in ${keys[@]}; do
-    resp=$(etcd/value $child $peers)
-    if [ $? -eq 0 ]; then
-      values=("${values[@]}" $resp)
+    local keys
+    local code
+    keys=$(etcd/keys $key $peers)
+    if [ $? -gt 0 ] ; then
+        return 1;
     fi
-  done
-  echo "${values[@]}"
+
+    local values=()
+    local resp
+    for child in ${keys[@]}; do
+        resp=$(etcd/value $child $peers)
+        if [ $? -eq 0 ]; then
+            values=("${values[@]}" $resp)
+        fi
+    done
+    echo "${values[@]}"
+}
+
+# Sets/puts a key/value pair into Etcd.
+#   args: KEY - the key to set;
+#         VALUE - the value to set;
+#         PEERS - (optional) the list of Etcd peers to use.
+etcd/put () {
+    local key=$1
+    local value=$2
+    local peers=${3:-$ETCDCTL_PEERS}
+
+    if [ -n $peers ]; then
+        etcdctl --peers "$peers" set "$key" "$value" >/dev/null 2>&1
+    else
+        etcdctl set "$key" "$value" >/dev/null 2>&1
+    fi
+    if [ $? -ne 0 ]; then
+        echo "Failed to set '$key' [$?]!" >&2
+        return 1
+    fi
+}
+
+# Initializes the Etcd directory for use with this service
+#   args: NS - the Etcd namespace to initialize
+#   returns: nothing
+etcd/init () {
+    local ns=$1
+    $(etcdctl ls "$ns" >/dev/null 2>&1)
+    if [ $? -ne 0 ]; then
+        _log "Dir ${ns} not found, creating one..."
+        etcdctl mkdir "$ns" 2>&1 >/dev/null
+    fi
+}
+
+etcd/mkdir () {
+    local ns=$1
+    local name=$2
+    # Create directory?
+    etcdctl ls "$ns/$name" >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        etcdctl mkdir "$ns/$name" >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            _log "Failed to create directory..."
+            exit $EPUBLISH_FAILED
+        fi
+    fi
 }
 
 #etcd/values /inaetics/apt-cacher-service
